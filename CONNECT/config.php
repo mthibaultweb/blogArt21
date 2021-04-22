9ae45c0379025e3d028ceb1c726a6ec5964ce554
mthibault006 <thibault.maxime@outlook.com>
2021-04-22 20:54:53 +0200

    CONNECT BDD

--- CONNECT/config.php
+++ CONNECT/config.php
@@ -1,14 +1,15 @@
 <?
 
   // nom de votre serveur (ou 127.0.0.1)
-  $hostBD = "plateforme-mmi.iut.u-bordeaux-montaigne.fr";
+  $hostBD = "db822209643.hosting-data.io";
   // nom BD
-  $nomBD = "wp_user07";
+  $nomBD = "db822209643";
   // Serveur
   // Avec encodage UTF8
   $serverBD = "mysql:dbname=$nomBD;host=$hostBD;charset=utf8";
 
   // nom utilisateur de connexion à la BDD
-  $userBD = 'user_db_07';         // Votre login
+  $userBD = 'dbo822209643';         // Votre login
   // mot de passe de connexion à la BDD
   $passBD = 'mmi-2020';         // Votre Pass
+?>
